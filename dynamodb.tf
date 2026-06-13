0f2db2f57d9b3610bfa923a8a6c2ffd5a2a2a428
narendar <narendar4100@gmail.com>
2026-06-13 16:50:49 +0530

    removed duplicate code

--- dynamodb.tf
+++ dynamodb.tf
@@ -9,14 +9,4 @@ module "dynamodb_incident_logs" {
     Environment = var.environment
   }
 }
-module "dynamodb_incident_logs" {
-  source = "../modules/dynamodb"
-  table_name = var.ddb_table_name
-  hash_key = "IncidentId"
-  hash_key_type = "S"
-  range_key = "Timestamp"
-  range_key_type = "N"
-  tags = {
-    Environment = var.environment
-  }
-}
+
